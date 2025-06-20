35914694248c94c8510fd88aefb348c94fa8fa79
Rachit Gupta <guptarachit2711@gmail.com>
2025-06-20 23:54:04 +0530

    changes are made

--- 06_mysqlwithexpress/index.js
+++ 06_mysqlwithexpress/index.js
@@ -1,9 +1,41 @@
-// learning to use faker 
-// see file insertdata with placeholder to enter data with a variable;
+// all the little things which we learned to do this project are in the learning folder so go through it 
+
+//now in this file we create a server for our project
+
+/* 
+1. we have already entered data of 103 users by using faker (to see how go in learning folder)
+2.now we create a mini project to show our suers ,delete ,add ect all this using a data base
+*/
+
+
+const express=require('express');
+const app=express();
+
+const port =8080;
+
+let methodOverride = require('method-override');  //requering override package
+app.use(methodOverride('_method')); //telling condition to override
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+const path=require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"public")));
+
+
+app.listen(port, ()=>{
+  console.log("listining on port 8080");
+})
+
 
 const { faker } = require('@faker-js/faker');  // require faker
 
 const mysql = require('mysql2'); //require my sql
+const { log } = require('console');
 
 const connection = mysql.createConnection({  // creating connection
   host: 'localhost',
@@ -12,33 +44,85 @@ const connection = mysql.createConnection({  // creating connection
   password:'1411471'
 });
 
-// creating a variable and writing all the queries in it then pass that variable in the 
-//connect.query function (its just a better way of writing);
-// using faker to enter all the data in the table
-let q='INSERT INTO  user (id,username,email,passward) VALUES ?';
 
-let data=[];
-
-
-try{
-    connection.query(q,(err,result)=>{  //writing queries & handling err 
-        if(err) throw(err);
-        console.log(result);
-        console.log(result.length);
-})
-}catch(err){
+app.get("/",(req,res)=>{
+  // res.send("home page");
+  let q=`SELECT COUNT(*) FROM user`;
+  try{
+    connection.query(q,(err,result)=>{
+      if(err) throw err;
+      
+      // console.log(result[0]["COUNT(*)"]);
+      let countt=result[0]["COUNT(*)"];
+       res.render("index.ejs",{countt});
+    })
+  }catch(err){
     console.log(err);
-}
-connection.end(); // automatically end the connection
+    res.send("some error ):");
+  }
+
+})
+
+app.get("/user",(req,res)=>{
+  
+  try{
+    let q="select id,username,email from user";
+    connection.query(q,(err,result)=>{
+      if(err) throw err;
+      // console.log(result);
+      let allusersdata=result;
+      let count=result.length;
+      console.log(count);
+      
+      // res.send("this will show all the users");
+      res.render("showdata.ejs",{allusersdata,count});
+    })
+  }catch(err){
+    res.send("some error occured in showing all the users");
+  }
+})
 
 
-let createRandomUser = ()=> {
-  return [
-     faker.string.uuid(),
-     faker.internet.username(), // before version 9.1.0, use userName()
-     faker.internet.email(),
-     faker.internet.password(),
-  ];
-}
 
-//console.log(createRandomUser());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------
+// garbage 
+// try{
+//     connection.query(q,(err,result)=>{  //writing queries & handling err 
+//         if(err) throw(err);
+//         console.log(result);
+//         console.log(result.length);
+// })
+// }catch(err){
+//     console.log(err);
+// }
+// connection.end(); // automatically end the connection
+
+
+// let createRandomUser = ()=> {
+//   return [
+//      faker.string.uuid(),
+//      faker.internet.username(), // before version 9.1.0, use userName()
+//      faker.internet.email(),
+//      faker.internet.password(),
+//   ];
+// }
+
+// //console.log(createRandomUser());
